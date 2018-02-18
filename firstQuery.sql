9501dc68c14dc152c4b6c5d1e6323f79fe2e127a
jaymemika <35896099+jaymemika@users.noreply.github.com>
2018-02-18 18:45:24 -0500

    Add files via upload

---
+++ firstQuery.sql
@@ -0,0 +1,8 @@
+select CommonName, Habitat from animals Where habitat='China';
+
+update Medicines
+set Price=2.00
+where Company='Major Pharmaceuticals';
+
+insert into Zoos (Name, Address, City, State)
+values ('Turtle Back', '560 Northfield Ave', 'West Orange', 'NJ');
